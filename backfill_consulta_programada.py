9c37c625728e80d7b543e786b193e029769848fd
Fernando Masumoto <fernando.masumoto@gmail.com>
2024-12-20 11:59:48 -0300

    .

--- backfill_consulta_programada.py
+++ backfill_consulta_programada.py
@@ -2,12 +2,8 @@ import pendulum
 
 RESOURCE_NAME = 'RESOURCE_NAME'
 
-run_time = pendulum.datetime(2024, 1, 1)
-end_dt = pendulum.datetime(2024, 1, 5)
-
-command = f"bq mk --transfer_run --run_time='{run_time.isoformat()}' {RESOURCE_NAME}"
-subprocess.run(command.split(' '), capture_output=True)
-
+run_time = pendulum.datetime(2024, 1, 5)
+end_dt = pendulum.datetime(2024, 2, 1)
 
 while run_time < end_dt:
     command = f"bq mk --transfer_run --run_time='{run_time.isoformat()}' {RESOURCE_NAME}"
