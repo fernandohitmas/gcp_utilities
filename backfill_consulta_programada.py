f8132df0cfd841bdb7c91c92c220a112c950b0c1
Fernando Masumoto <fernando.masumoto@gmail.com>
2024-12-20 12:10:06 -0300

    Added Pyperclip to pass all the commands into the clipboard, instead of printing in the terminal

--- backfill_consulta_programada.py
+++ backfill_consulta_programada.py
@@ -1,14 +1,16 @@
 import pendulum 
-
+import pyperclip
 RESOURCE_NAME = 'RESOURCE_NAME'
 
-run_time = pendulum.datetime(2024, 1, 5)
-end_dt = pendulum.datetime(2024, 2, 1)
+run_time = pendulum.datetime(2024, 2, 1)
+end_dt = pendulum.datetime(2024, 12, 8)
 
-while run_time < end_dt:
-    command = f"bq mk --transfer_run --run_time='{run_time.isoformat()}' {RESOURCE_NAME}"
+command = ''
+
+while run_time <= end_dt:
+    command = command + f"bq mk --transfer_run --run_time='{run_time.isoformat()}' {RESOURCE_NAME}\n"
     
-    print(command)
-
     # Go to next day
     run_time = run_time.add(days=1)
+
+pyperclip.copy(command)
