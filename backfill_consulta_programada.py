9ac2cf35283b2871e00c4b941f51efad32e390c2
Fernando Masumoto <fernando.masumoto@gmail.com>
2024-12-20 11:57:58 -0300

    bq cli commands cannot be executed through subprocess library

--- backfill_consulta_programada.py
+++ backfill_consulta_programada.py
@@ -1,4 +1,3 @@
-import subprocess
 import pendulum 
 
 RESOURCE_NAME = 'RESOURCE_NAME'
@@ -6,10 +5,14 @@ RESOURCE_NAME = 'RESOURCE_NAME'
 run_time = pendulum.datetime(2024, 1, 1)
 end_dt = pendulum.datetime(2024, 1, 5)
 
+command = f"bq mk --transfer_run --run_time='{run_time.isoformat()}' {RESOURCE_NAME}"
+subprocess.run(command.split(' '), capture_output=True)
+
+
 while run_time < end_dt:
     command = f"bq mk --transfer_run --run_time='{run_time.isoformat()}' {RESOURCE_NAME}"
     
-    subprocess.run(command.split(' '), capture_output=True)
+    print(command)
 
     # Go to next day
     run_time = run_time.add(days=1)
